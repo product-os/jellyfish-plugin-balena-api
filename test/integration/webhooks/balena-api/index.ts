06b371129fbecb0f26b7cffd07543b38bfdae079
Josh Bowling <josh@balena.io>
2021-11-20 22:39:26 +0900

    Remove license headers
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- test/integration/webhooks/balena-api/index.ts
+++ test/integration/webhooks/balena-api/index.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Balena.io - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
 export default {
 	'new-user-invalid-ip': {
 		expected: require('./new-user-invalid-ip/expected.json'),
